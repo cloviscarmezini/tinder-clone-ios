926a24acad1ca281c6749363da6d707664ea7bcd
Clovis Carmezini <cloviscarmezini14@gmail.com>
2023-04-18 21:42:37 -0300

    Initial Commit

---
+++ tinder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tinder
+//
+//  Created by Clovis Carmezini on 18/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
