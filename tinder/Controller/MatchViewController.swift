c49921d91e00503fb60c605cbd99de5d7d09d28d
cloviscarmezini <cloviscarmezini14@gmail.com>
2024-02-07 22:52:55 -0300

    feat: added send message button and return button

--- tinder/Controller/MatchViewController.swift
+++ tinder/Controller/MatchViewController.swift
@@ -23,7 +23,33 @@ class MatchViewController: UIViewController {
         textField.textColor = .darkText
         textField.returnKeyType = .go
         
+        textField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 16, height: 0))
+        textField.leftViewMode = .always
+        
+        textField.rightView = UIView(frame: CGRect(x: 0, y: 0, width: 90, height: 0))
+        textField.rightViewMode = .always
+        
         return textField
+    }()
+    
+    let submitButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Enviar", for: .normal)
+        button.setTitleColor(UIColor(red: 62/255, green: 163/255, blue: 255/255, alpha: 1), for: .normal)
+        
+        button.titleLabel?.font = .boldSystemFont(ofSize: 16)
+
+        return button
+    }()
+    
+    let backButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Voltar para o Tinder", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        
+        button.titleLabel?.font = .systemFont(ofSize: 16)
+
+        return button
     }()
     
     override func viewDidLoad() {
@@ -40,7 +66,16 @@ class MatchViewController: UIViewController {
         likeImageView.heightAnchor.constraint(equalToConstant: 44).isActive = true
         likeImageView.contentMode = .scaleAspectFit
         
-        let stackView = UIStackView(arrangedSubviews: [likeImageView, labelMessage, messageInput])
+        messageInput.addSubview(submitButton)
+        submitButton.fill(
+            top: messageInput.topAnchor,
+            leading: nil,
+            trailing: messageInput.trailingAnchor,
+            bottom: messageInput.bottomAnchor,
+            padding: .init(top: 0, left: 0, bottom: 0, right: 16)
+        )
+        
+        let stackView = UIStackView(arrangedSubviews: [likeImageView, labelMessage, messageInput, backButton])
         stackView.axis = .vertical
         stackView.spacing = 16
         
